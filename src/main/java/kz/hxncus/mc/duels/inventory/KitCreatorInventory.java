b1ff51f6bed7648fe3c9da79cfd74dd46a30bc2b
Hxncus0 <aika0281@gmail.com>
2023-07-02 03:21:26 +0600

    Trying to reduce code complexity to zero

--- src/main/java/kz/hxncus/mc/duels/inventory/KitCreatorInventory.java
+++ src/main/java/kz/hxncus/mc/duels/inventory/KitCreatorInventory.java
@@ -1,6 +1,7 @@
 package kz.hxncus.mc.duels.inventory;
 
 import kz.hxncus.mc.duels.methods.ItemStacker;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
@@ -8,7 +9,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class KitCreatorInventory {
     public static Inventory getInventory() {
-        Inventory inventory = Bukkit.createInventory(null, 45, "§cСоздать кит");
+        Inventory inventory = Bukkit.createInventory(null, 45, Component.text("§cСоздать кит"));
 
         inventory.setItem(22, ItemStacker.setDisplayName(new ItemStack(Material.BEDROCK), "§eКреатив"));
         inventory.setItem(20, ItemStacker.setDisplayName(new ItemStack(Material.NAME_TAG), "§eНазвание кита"));
