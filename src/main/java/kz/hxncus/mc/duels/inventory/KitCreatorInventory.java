d2012d617cbdd67c88d39c512412e2b019255e25
Hxncus0 <aika0281@gmail.com>
2023-07-02 03:15:22 +0600

    Trying to reduce code complexity to zero

--- src/main/java/kz/hxncus/mc/duels/inventory/KitCreatorInventory.java
+++ src/main/java/kz/hxncus/mc/duels/inventory/KitCreatorInventory.java
@@ -9,7 +9,21 @@ import org.bukkit.inventory.ItemStack;
 public class KitCreatorInventory {
     public static Inventory getInventory() {
         Inventory inventory = Bukkit.createInventory(null, 45, "§cСоздать кит");
-        inventory.setItem(22, ItemStacker.setDisplayName(new ItemStack(Material.BEDROCK), "§eКреатив"));
+        inventory.setItem(22, createBedrockItem());
+        inventory.setItem(20, createNameTagItem());
+        inventory.setItem(24, createSaveKitItem());
         return inventory;
+    }
+
+    private static ItemStack createBedrockItem() {
+        return ItemStacker.setDisplayName(new ItemStack(Material.BEDROCK), "§eКреатив");
+    }
+
+    private static ItemStack createNameTagItem() {
+        return ItemStacker.setDisplayName(new ItemStack(Material.NAME_TAG), "§eНазвание кита");
+    }
+
+    private static ItemStack createSaveKitItem() {
+        return ItemStacker.setDisplayName(new ItemStack(Material.LIME_WOOL), "§aСохранить кит");
     }
 }
