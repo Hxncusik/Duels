96093f37d28fd527dc55ae91edf72d5b0af174d8
Hxncus0 <aika0281@gmail.com>
2023-07-03 17:17:18 +0600

    Created for save kit name placed by player

---
+++ src/main/java/kz/hxncus/mc/duels/kits/PlayerManager.java
@@ -0,0 +1,31 @@
+package kz.hxncus.mc.duels.kits;
+
+import kz.hxncus.mc.duels.Duels;
+
+import java.util.Map;
+
+public class PlayerManager {
+    private final Map<String, PlayerManager> playerManagerMap = Duels.getPlayerManagerMap();
+    private final String playerName;
+    private String kitName;
+    private boolean isNaming;
+    public PlayerManager(String playerName, String name, boolean isNaming) {
+        this.playerName = playerName;
+        this.kitName = name;
+        this.isNaming = isNaming;
+    }
+    public String getKitName() {
+        return kitName;
+    }
+    public void setKitName(String name) {
+        this.kitName = name;
+        playerManagerMap.put(playerName, this);
+    }
+    public boolean isNaming() {
+        return isNaming;
+    }
+    public void setNaming(boolean naming) {
+        isNaming = naming;
+        playerManagerMap.put(playerName, this);
+    }
+}
